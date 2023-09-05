1934a66982ecced8e8fc294122ddad4adc0154b0
Volatus <volatusCoder@outlook.com>
2023-09-05 12:57:39 -0300

    Quartz sync: Sep 5, 2023, 12:57 PM

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -7,13 +7,17 @@ export default (() => {
     const text = fileData.text
     if (text) {
       const segments: string[] = []
-      const { text: timeTaken, words: _words } = readingTime(text)
+      const { words: _words, minutes: timeTaken } = readingTime(text)
 
       if (fileData.dates) {
         segments.push(formatDate(getDate(cfg, fileData)!))
       }
 
-      segments.push(timeTaken)
+      const displayedTime = Math.ceil(timeTaken.toFixed(2))
+      const timeScale = displayedTime > 1.0 ? 'minutos' : 'minuto'
+      const timeText = 'leitura de ' + displayedTime + ' ' + timeScale
+
+      segments.push(timeText)
       return <p class="content-meta">{segments.join(", ")}</p>
     } else {
       return null
